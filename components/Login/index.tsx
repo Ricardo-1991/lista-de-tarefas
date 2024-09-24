e639fb7e5e343769d5736cfe15a0f9b669a7e089
Ricardo <ricardo.dev1991@gmail.com>
2024-09-23 22:16:41 -0300

    feat: Adicionando interface de tela de login.

---
+++ components/Login/index.tsx
@@ -0,0 +1,18 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native';
+import { LoginButton, LoginButtonText, LoginContainer, Title } from './style';
+
+
+
+export function Login() {
+    return (
+        <LoginContainer>
+            <View>
+                <Title>Aqui você terá sua lista de tarefas na palma da sua mão!</Title>
+            </View>
+            <Image source={require('../../assets/loginImage.png')} style={{width: 300, height: 300}} />
+            <LoginButton>
+                <LoginButtonText>Entrar</LoginButtonText>
+            </LoginButton>
+        </LoginContainer>
+    );
+}
